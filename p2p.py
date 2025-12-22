a0dfcdd297d23515a15748ddd7c90581a4c8eb4e
devansh-pujari-tech <devanshpujari07@gmail.com>
2025-12-22 12:08:54 +0530

    Add files via upload

---
+++ p2p.py
@@ -0,0 +1,10 @@
+#Problem Statement: Given an integer array nums, rotate the array to the left by one.
+
+n=int(input())
+arr=list(map(int,input().split()))
+temp=arr[0]
+for i in range(1,n):
+    arr[i-1]=arr[i]
+arr[n-1]=temp
+for x in arr:
+    print(x,end=" ")
